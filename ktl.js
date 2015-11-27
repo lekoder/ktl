30723a8acf67c772773d4b2be97b962ddbcfb0d6
Koder <mariusz.chwalba@cashbill.pl>
2015-11-27 10:36:04 +0100

    Adds support for empty tags

--- ktl.js
+++ ktl.js
@@ -1,15 +1,29 @@
+/*
+ * KLT uses tags:
+ * 
+ * {{ var }} - variable
+ * {{# array }} {{ val }} {{#}} - array/object iteration
+ */
+
 var ktl = function (template) {
 
     var body = "{ with(_||{}) return '" + 
             template
-                .replace(/\\/g,'\\\\')
-                .replace(/'/g,"\\'")
-                .replace(/\n/g,"\\n")
-                .replace(/\r/g,"\\r")
+                .replace(/\\/g,'\\\\')                  // escape \
+                .replace(/'/g,"\\'")                    // escape '
+                .replace(/\n/g,"\\n")                   // escape \n
+                .replace(/\r/g,"\\r")                   // escape \r
+                
+                .replace(/\{\{[^#?]([^}]*)\}\}/g, "'+(typeof($1)!='undefined'?$1:'')+'")   // parse {{ tag }}
+                
         + "'; }";
-    try {
+        
+    try 
+    {
         var parser = new Function('_', body);
-    } catch (e) {
+    }
+    catch (e) 
+    {
         console.err("Could not compile template: " + e);
     }
 
--- test/ktl.js
+++ test/ktl.js
@@ -38,5 +38,43 @@ describe("ktl", function () {
         var template = "r \r r \r r \r";
         ktl(template)().should.be.equal(template);
     });
+    
+    it("replaces {{ tag }} with value", function() {
+       ktl("string {{ tag }} string")
+          ({tag:'asd'}).should.be.equal(
+              "string asd string"
+          );
+    });
+    it("replaces more than one tag", function() {
+       ktl("string {{ tag }} string {{ bong }}")
+          ({tag:'asd',bong:'wtf'}).should.be.equal(
+              "string asd string wtf"
+          );
+    });
+    
+    it("replaces {{ tag.subtag }} with value", function() {
+       ktl("string {{ tag.subtag }} string")
+          ({tag:{subtag:'asd'}}).should.be.equal(
+              "string asd string"
+          );
+    });
+    
+    it("replaces missing values with empty strings", function() {
+       ktl("string {{ tag }} string")
+          ({}).should.be.equal(
+              "string  string"
+          );
+    });
+    
+    
+    
+    it("iterates over {{# array }} / {{#}} tag", function() {
+        var template = "array:{{# a }}v:{{ val }};{{#}}";
+        var data = [{val:1},{val:2}];
+        var expect = "array:v:1;v:2;";
+        
+        ktl(template)(data).should.be.equal(expect);
+    });
+    
 
 });
