256518046847a4e0ba58d5e786c481731c55e495
Koder <mariusz.chwalba@cashbill.pl>
2015-12-01 15:57:13 +0100

    Work snapshot

--- ktl.js
+++ ktl.js
@@ -7,45 +7,85 @@
  * {{? condition}} data {{?}} - conditional
  */
 
-function makeCondition(tag, arg, content) {
-    return "\"+(function() { try { return("+arg+"?(\""+content+"\"):\"\"); } catch(e){ return ''} } )()+\"";
+var detlimer = /{{|}}/
+var nestingTokens = {
+    '#': function (tokens) {
+        // build an iteration
+        var array = tokens[0].substring(1);
+        return array + ".map(function(_,$){ try{ " + build(tokens.splice(1)) + "  } catch(e) { return \"\"}  } ).join(\"\")";
+
+    },
+    '?': function (tokens) {
+        // build an condition
+        console.log("condition",tokens);
+        var condition = tokens[0].substring(1);
+        return condition + "? (function() { "+build(tokens.splice(1))+" })() : \"\"";
+    }
 }
 
-function makeIterator(tag, iterateOver, withTempate) {
-    var parser = ktl(JSON.parse("\""+withTempate+"\"")); // template is already escaped
-    return "\"+("
-        + "typeof("+iterateOver+")!==\"undefined\" &&"
-        + iterateOver + ".map && "    // iterate only over arrays
-        + iterateOver + ".map("
-        + parser.toString()             // parser for subtemplate
-        + ").join(\"\") || \"\")+\"";    // default to empty string
+function build(tokens) {
+    var body = [];
+    var idx;
+
+    for (idx = 0; idx < tokens.length; idx += 2) {
+        body.push(JSON.stringify(tokens[idx]));
+        var token = tokens[idx + 1];
+        if (token) {
+            var nesting = nestingTokens[token[0]];
+            if (nesting) {
+                if (token.length > 1) {
+                    var depth = 1;
+                    var nestor = token[0];
+                    var ahead = idx + 1;
+
+                    do {
+                        ahead += 2;
+                        if (ahead >= tokens.length) break;
+                        if (tokens[ahead][0] == nestor) {
+
+                            if (tokens[ahead].length === 1) {
+                                depth -= 1;
+                            } else {
+                                depth += 1;
+                            }
+                        }
+                    } while (depth > 0);
+                    if (depth > 0) {
+                        throw new SyntaxError("Missing closing " + nestor);
+                    }
+                    body.push("(" + nesting(tokens.slice(idx + 1, ahead)) + ")");
+
+                    idx = ahead;
+                }
+                else {
+                    throw new SyntaxError("Unexpeced closing tag: " + token);
+                }
+            }
+            else {
+                body.push("(typeof(" + token + ")!==\"undefined\"?" + token + ":\"\")");
+            }
+        }
+    }
+
+
+    var source = "{ with(_||{}) { return " + body.join("+") + "; } }";
+    console.log(source);
+    return source;
 }
 
-function escape(str) {
-    var e=JSON.stringify(str);
-    return e.substring(1, e.length-1);
-
+function compile(tokens) {
+    return new Function("_,$", build(tokens));
 }
 
 function ktl(template) {
-    var body = "with(_ instanceof Object ? _ : {}) return \"" +
-            template
-            .replace(/[^{}]+({{|$)/g,escape)
-            .replace(/\{\{\#([^}]+)\}\}([\s\S]*)\{\{\#\}\}/gm, makeIterator) // create a iterator {{# }}    
-            .replace(/\{\{\?([^}]+)\}\}([\s\S]*?)\{\{\?\}\}/gm, makeCondition) // create a condition {{? }}
-            .replace(/\{\{([^#?][^}]*)\}\}/g, "\"+(typeof($1)!=\"undefined\"?$1:\"\")+\"")   // parse {{ tag }}
-                
-        + "\";";
 
+    console.log(template);
     try {
-        var parser = new Function("_,$", "{ " + body + " }");
+        return compile(template.split(detlimer));
     }
     catch (e) {
         console.error("Could not compile template: " + e);
-        console.trace(body);
     }
-
-    return parser;
 }
 
 module.exports = ktl;
